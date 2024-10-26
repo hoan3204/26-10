3d7beb4856d43e40ffebb1637e6d0b8348132d75
hoan3204 <hoan25913@gmail.com>
2024-10-26 14:38:52 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLookup = new Button();
+            txtStudentID = new TextBox();
+            label1 = new Label();
+            lblResult = new Label();
+            SuspendLayout();
+            // 
+            // btnLookup
+            // 
+            btnLookup.Location = new Point(381, 148);
+            btnLookup.Name = "btnLookup";
+            btnLookup.Size = new Size(94, 29);
+            btnLookup.TabIndex = 0;
+            btnLookup.Text = "Tra cứu";
+            btnLookup.UseVisualStyleBackColor = true;
+            btnLookup.Click += btnLookup_Click;
+            // 
+            // txtStudentID
+            // 
+            txtStudentID.Location = new Point(153, 148);
+            txtStudentID.Name = "txtStudentID";
+            txtStudentID.Size = new Size(164, 27);
+            txtStudentID.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(53, 148);
+            label1.Name = "label1";
+            label1.Size = new Size(94, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Mã sinh viên:";
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(176, 252);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(12, 20);
+            lblResult.TabIndex = 3;
+            lblResult.Text = ".";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblResult);
+            Controls.Add(label1);
+            Controls.Add(txtStudentID);
+            Controls.Add(btnLookup);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLookup;
+        private TextBox txtStudentID;
+        private Label label1;
+        private Label lblResult;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,40 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private Dictionary<string, string> studentScores = new Dictionary<string, string>()
+        {
+            { "22810310375", "Điểm: 8.5" },
+            { "22810310360", "Điểm: 7.0" },
+            { "22810310365", "Điểm: 9.0" } 
+        };
+
+        
+
+        private void btnLookup_Click(object sender, EventArgs e)
+        {
+            // Lấy mã sinh viên từ TextBox
+            string studentID = txtStudentID.Text.Trim();
+
+            // Kiểm tra xem mã sinh viên có trong danh sách không
+            if (studentScores.TryGetValue(studentID, out string score))
+            {
+                lblResult.Text = $"Thông tin sinh viên:\nMã: {studentID}\n{score}";
+            }
+            else
+            {
+                lblResult.Text = "Không tìm thấy sinh viên.";
+            }
+        }
+
+        }
+}
